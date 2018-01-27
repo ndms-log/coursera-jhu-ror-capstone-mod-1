4cfb61b5bbacc6b0a64ccac7f5af1843a6abbd29
Nils <ndms.learning@gmail.com>
2018-01-27 15:46:11 +0000

    rdbms-backed resource City added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  scope :api, defaults: {format: :json} do
+    resources :cities, except: [:new, :edit]
+  end
+  
   get '/ui' => 'ui#index'
   get '/ui#' => 'ui#index'
   root 'ui#index'
