89b078cbf409e20e63a57cb1dd536ef668265c30
Nils <ndms.learning@gmail.com>
2018-01-28 12:13:01 +0000

    MongoDB-backed resource State added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
+  
   scope :api, defaults: {format: :json} do
     resources :cities, except: [:new, :edit]
+    resources :states, except: [:new, :edit]
   end
   
   get '/ui' => 'ui#index'
